49835e8621b8b22a6404b008ac2248ce130fad31
xeviff <xavier.fontanet@edreamsodigeo.com>
2022-07-13 16:32:31 +0200

    small bug :)

--- src/main/shell_scripts/java_radarr-fix.sh
+++ src/main/shell_scripts/java_radarr-fix.sh
@@ -2,7 +2,7 @@ echo "> renaming previous log file for java trick..."
 mv ~/logs/java/radarr-fix_done.log ~/logs/java/radarr-fix_old.log 
 echo "> Done"
 echo "> docker-compose down and up the AfterFilebotMoveProcessor"
-cd ""
+cd "$JAVA_RADARRFIX_PROJECT_PATH"
 docker-compose down
 echo "> down complete, now up"
 docker-compose up > ~/logs/java/radarr-fix.log
